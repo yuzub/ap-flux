969faba395bbf3e94b65f1862659ecd7e5e4d3af
Yuriy Zubaha <angular2amconsoft@gmail.com>
2020-07-07 00:33:20 +0300

    added CourseForm component

---
+++ src/components/CourseForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import TextInput from './common/TextInput';
+
+function CourseForm(props) {
+  return (
+    <form>
+      <TextInput
+        id="title"
+        label="Title"
+        onChange={props.onChange}
+        name="title"
+        value={props.course.title}
+      />
+
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            onChange={props.onChange}
+            value={props.course.authorId || ''}
+            className="form-control"
+          >
+            <option value="" />
+            <option value="1">Cory House</option>
+            <option value="2">Scott Allen</option>
+          </select>
+        </div>
+      </div>
+
+      <TextInput
+        id="category"
+        label="Category"
+        name="category"
+        onChange={props.onChange}
+        value={props.course.category}
+      />
+
+      <input type="submit" value="Save" className="btn btn-primary" />
+    </form>
+  );
+}
+
+export default CourseForm;
--- src/pages/ManageCoursePage.js
+++ src/pages/ManageCoursePage.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Prompt } from 'react-router-dom';
+import CourseForm from '../components/CourseForm';
 
 const ManageCoursePage = (props) => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: '',
+    title: '',
+    authorId: null,
+    category: '',
+  });
+
+  //   function handleChange(event) {
+  //     const updatedCourse = {
+  //       ...course,
+  //       [event.target.name]: event.target.value,
+  //     };
+  //     // updatedCourse.title = event.target.value;
+  //     setCourse(updatedCourse);
+  //   }
+
+  function handleChange({ target }) {
+    // const target = event.target;
+    // const { target } = event;
+    const updatedCourse = {
+      ...course,
+      [target.name]: target.value,
+    };
+    setCourse(updatedCourse);
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
       <Prompt when={true} message="Are you sure you want to leave?" />
+      <CourseForm course={course} onChange={handleChange} />
       {props.match.params.slug}
     </>
   );
